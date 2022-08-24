312819ca06b4a2c383d5ca50be4aa4f2e887d018
Mert Savaş <mert.savas@d-teknoloji.com.tr>
2022-08-24 14:26:04 +0300

    added const tshirt sizes

---
+++ AzureDevopsBacklog/Contants/TShirtSizes.cs
@@ -0,0 +1,11 @@
+﻿namespace AzureDevopsBacklog.Contants
+{
+    public static class TShirtSizes
+    {
+        public const string XXSmall = "XX-Small";
+        public const string XSmall = "X-Small";
+        public const string Small = "Small";
+        public const string Medium = "Medium";
+        public const string Large = "Large";
+    }
+}
