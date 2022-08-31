8d10ddda1c9927a7eb9144137eb134d911adc124
Mert Savaş <mert.savas@d-teknoloji.com.tr>
2022-08-31 13:14:18 +0300

    added WorkItemService for handling WorkItemController

--- AzureDevopsBacklog/Application/Models/ResponseModels/BaseResponseModel.cs
+++ AzureDevopsBacklog/Application/Models/ResponseModels/BaseResponseModel.cs
@@ -2,6 +2,17 @@
 {
     public class BaseResponseModel<TModel>
     {
+        public BaseResponseModel()
+        {
+        }
+
+        public BaseResponseModel(TModel data, bool isSucceeded, string responseMessage)
+        {
+            Data = data;
+            ResponseMessage = responseMessage;
+            IsSucceeded = isSucceeded;
+        }
+
         public TModel Data { get; set; }
         public string ResponseMessage { get; set; }
         public bool IsSucceeded { get; set; }
