be18c087df9a95636324de4fe03bc1bd211c5323
Mert Savaş <mert.savas@d-teknoloji.com.tr>
2022-09-24 18:36:22 +0300

    edited report messages as html

--- AzureDevopsBacklog/Contants/ReportMessages.cs
+++ AzureDevopsBacklog/Contants/ReportMessages.cs
@@ -2,10 +2,10 @@
 {
     public static class ReportMessages
     {
-        public const string InCompleteWorkItemMessage = "Şuana kadar tamamlanmamış maddeleriniz bulunmaktadır.\n\n";
+        public const string InCompleteWorkItemMessage = "Şuana kadar tamamlanmamış maddeleriniz bulunmaktadır.</br></br> ";
         public static string SprintReminding(string sprintTag) => $"Sprint({sprintTag}) Bilgilendirilmesi";
-        public static string ReportDescription(double effort) => $"Haftalık efordan({effort}) az iş tamamlamış kişiler ve tamamlanmamış maddeleri aşağıdaki gibidir.\n\n";
-        public static string UserInfo(string uniqueName, int workItemCount) => $"<p style='color:red'>Kişi: {uniqueName}({workItemCount})</p> \n";
-        public static string InCompleteWorkItemMessageTemplate(string baseUrl, string title, int id) => $"Başlık: {title} \n Url: {RemoteUrls.GetWorkItemViewUrl(baseUrl, id)} \n\n";
+        public static string ReportDescription(double effort) => $"Haftalık efordan({effort}) az iş tamamlamış kişiler ve tamamlanmamış maddeleri aşağıdaki gibidir. </br></br> ";
+        public static string UserInfo(string uniqueName, int workItemCount) => $"<p style='color:red'>Kişi: {uniqueName}({workItemCount})</p> </br>";
+        public static string InCompleteWorkItemMessageTemplate(string baseUrl, string title, int id) => $"Başlık: {title} </br> Url: {RemoteUrls.GetWorkItemViewUrl(baseUrl, id)} </br></br> ";
     }
 }
